10b64bd60ab8319ec777cce1928236a964b206cd
yawyt <yawyt@users.noreply.github.com>
2016-02-27 13:13:43 +0000

    project step3

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require ('http');
 
-var server =http.createServer();
+var message = "I am so happy to be part of Node Girls Workshop!"
+
+function handler (requests, response) {
+  response.writeHead(200, {"Content-Type": "text/html"})
+  response.write(message); //response body
+  response.end(); //finish response
+}
+
+
+var server =http.createServer(handler);
 
 server.listen(3000, function () {
 
